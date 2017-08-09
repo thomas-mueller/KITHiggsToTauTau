f2699ad086a76de796720364c30ffaa682a5501e
rfriese <raphael.friese@cern.ch>
2017-08-09 13:42:13 +0200

    adjust checkout recipe to KIT-CMS
    Conflicts:
    	scripts/checkout_packages.sh

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -14,7 +14,7 @@ git cms-addpkg CondFormats/JetMETObjects
 
 # From Kappa, only the DataFormats are needed
 # Mind that for certain skims, you need exactly the Kappa git tag that has been used for the production
-git clone https://github.com/KappaAnalysis/Kappa.git 
+git clone https://github.com/KIT-CMS/Kappa.git 
 cd Kappa
 echo docs/ >> .git/info/sparse-checkout
 echo DataFormats/ >> .git/info/sparse-checkout
@@ -23,18 +23,16 @@ git config core.sparsecheckout true
 git read-tree -mu HEAD
 cd ..
 
-git clone https://github.com/KappaAnalysis/KappaTools.git 
-git clone https://github.com/artus-analysis/Artus.git 
-git clone https://github.com/artus-analysis/Artus.wiki.git Artus/Core/doc/wiki
+git clone https://github.com/KIT-CMS/KappaTools.git -b master
+git clone https://github.com/KIT-CMS/Artus.git 
 
 # checkout KITHiggsToTauTau CMSSW analysis package
-git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau 
-git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau.wiki.git HiggsAnalysis/KITHiggsToTauTau/doc/wiki
+git clone https://github.com/KIT-CMS/KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau 
 #svn co https://ekptrac.physik.uni-karlsruhe.de/svn/KITHiggsToTauTau-auxiliaries/trunk HiggsAnalysis/KITHiggsToTauTau/auxiliaries
 
 # Svfit and HHKinFit
 # git clone https://github.com/veelken/SVfit_standalone.git TauAnalysis/SVfitStandalone -b HIG-16-006
-git clone https://github.com/CMSAachen3B/SVfit_standalone.git TauAnalysis/SVfitStandalone -b HIG-16-006
+git clone https://github.com/KIT-CMS/SVfit_standalone.git TauAnalysis/SVfitStandalone -b HIG-16-006
 git clone https://github.com/artus-analysis/HHKinFit2.git -b artus
 
 # Jet2Tau Fakes
@@ -46,9 +44,6 @@ cd $CMSSW_BASE/src/
 # EmuQCD Method
 git clone https://github.com/CMS-HTT/QCDModelingEMu.git HTT-utilities/QCDModelingEMu
 
-# Fit Package for tau polarisation
-git clone https://github.com/CMSAachen3B/SimpleFits.git -b artus_master
-
 # needed for plotting and statistical inference
 git clone https://github.com/cms-analysis/CombineHarvester.git CombineHarvester
 git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit -b 74x-root6
@@ -59,15 +54,8 @@ cd -
 # needed for error propagation e.g. in the background estimations
 git clone https://github.com/lebigot/uncertainties.git -b 2.4.6.1 HiggsAnalysis/KITHiggsToTauTau/python/uncertainties
 
-#FastBDT
-git clone https://github.com/artus-analysis/FastBDT.git
-cd $CMSSW_BASE/src/FastBDT
-cmake .
-make
-cd $CMSSW_BASE/src/
-
 # Grid-Control
-git clone https://github.com/grid-control/grid-control.git -b r1941
+git clone https://github.com/janekbechtel/grid-control.git -b r1941
 
 # modified fork from GC, needed for artusMergeOutputsWithGC.py. Use artusMergeOutputs.py with the --batch as alternative not needing this patch
 # git clone https://github.com/artus-analysis/grid-control.git 
